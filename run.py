47c0cce66676fc3718d55e55b76e36f9600de1b8
Rushikesh Bansode <rushikeshbansode777@gmail.com>
2025-11-19 19:05:14 +0530

    Added test data file to run,py to add some users

--- run.py
+++ run.py
@@ -10,5 +10,6 @@ def make_shell_context():
     return {'db': db, 'User': User, 'Task': Task, 'Comment': Comment}
 
 if __name__ == '__main__':
+    seed_data()
     app.run(debug=True, host='0.0.0.0', port=5000)
 
